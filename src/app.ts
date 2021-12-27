b15fbd6ac2b7fbe199ec18ffa31297b892ee2495
hsn8najafi <hsn8najafi@gmail.com>
2021-12-27 09:53:43 +0330

    Express.js Initialized

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 // import "dotenv/config";
 import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
 
 dotenv.config();
 
+const app: Application = express();
+
+// Main Route
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello");
+});
+
 const PORT = process.env.PORT;
-console.log(PORT);
+app.listen(PORT, () =>
+  console.log(`Server is Running on : http://${"127.0.0.1"}:${PORT}`)
+);
