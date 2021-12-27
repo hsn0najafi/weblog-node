fb85169a8351992dd5096517b7da57e80dbde128
hsn8najafi <hsn8najafi@gmail.com>
2021-12-27 19:14:30 +0330

    Remove Test Log

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const dataBaseConnection = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI!);
-    console.log(conn.connection.host);
   } catch (err) {
     console.log(err);
 
