ee4196e84e6a8342b59958b4b33a6c03e37cbdb9
hsn8najafi <hsn8najafi@gmail.com>
2021-12-27 09:56:16 +0330

    Set Default Hostname on Environment Variables

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.get("/", (_: Request, res: Response) => {
   res.send("Hello");
 });
 
-const PORT = process.env.PORT;
+const { PORT, HOST } = process.env;
 app.listen(PORT, () =>
-  console.log(`Server is Running on : http://${"127.0.0.1"}:${PORT}`)
+  console.log(`Server is Running on : http://${HOST}:${PORT}`)
 );
