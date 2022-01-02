9cb76e1a86cb17f06dfb536f4fe8f977a522191a
hsn8najafi <hsn8najafi@gmail.com>
2022-01-02 10:27:56 +0330

    Signup Controller

---
+++ src/controllers/signup.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const signupController = (_: Request, res: Response) => {
+  res.render("pages/signup", {
+    pageTitle: "Signup",
+    message: "Register",
+    layout: "loginSignup",
+  });
+};
