283ece3affc6c22c2d9a82f8884ee19a32cd99c8
hsn8najafi <hsn8najafi@gmail.com>
2022-01-06 17:04:01 +0330

    Refactoring: Add Some Comment and Description

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
+/**
+ * Page: 'pages/home'
+ * Layout: 'layout' - This is Main Layout - Set Optional
+ * Params: ['pageTitle', 'message']
+ */
 export const homeController = (_: Request, res: Response) => {
   res.render("pages/home", {
     pageTitle: "Home",
