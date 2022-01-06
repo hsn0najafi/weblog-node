a7008421cafc02f32fcf87c2d1a68e8deb0daa9e
hsn8najafi <hsn8najafi@gmail.com>
2022-01-06 17:02:59 +0330

    Refactoring: Add Description

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
+/**
+ * Page: 'pages/dashboard'
+ * Layout: 'dashboard'
+ * Params: ['pageTitle', 'message']
+ */
 export const dashboardController = (_: Request, res: Response) => {
   res.render("pages/dashboard", {
     pageTitle: "Dashboard",
