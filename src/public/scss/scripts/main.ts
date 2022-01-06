392a0c5df1c020041fe39e2e637d325ef9e53a84
hsn8najafi <hsn8najafi@gmail.com>
2022-01-06 17:21:50 +0330

    classNameToggler Toggle Classes to Elements

--- src/public/scss/scripts/main.ts
+++ src/public/scss/scripts/main.ts
@@ -1,2 +1,13 @@
 const hamburgerButton = document.querySelector(".hamburgerButton");
 const hamburgerButton_lines = document.querySelectorAll(".hamburgerButton div");
+
+/**
+ * Add and Remove a ClassName to DOMElement
+ */
+const classNameToggler = (
+  targetElements: NodeListOf<Element>,
+  classNames: string[]
+) => {
+  console.log(targetElements);
+  console.log(classNames);
+};
