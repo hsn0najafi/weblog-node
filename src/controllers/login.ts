6ddf1f4b706b9d21b616e68d44b82f4484a2f09e
hsn8najafi <hsn8najafi@gmail.com>
2022-01-06 17:04:37 +0330

    Refactoring: Add Some Comment and Description

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
+/**
+ * Page: 'pages/login'
+ * Layout: 'loginSignup'
+ * Params: ['pageTitle', 'message']
+ */
 export const loginController = (_: Request, res: Response) => {
   res.render("pages/login", {
     pageTitle: "Login",
--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
+/**
+ * Page: 'pages/signup'
+ * Layout: 'loginSignup'
+ * Params: ['pageTitle', 'message']
+ */
 export const signupController = (_: Request, res: Response) => {
   res.render("pages/signup", {
     pageTitle: "Signup",
