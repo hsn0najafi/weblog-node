81a8e9f0b488ab45d1640a78e0ac4eb8286ad56f
hsn8najafi <hsn8najafi@gmail.com>
2022-01-08 18:06:00 +0330

    Using 'yup' InsteadOf 'fastest-validator'

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,52 +1,4 @@
 import { Request, Response } from "express";
-import FastestValidator from "fastest-validator";
-
-// Validator Schema And Error Messages
-const validator = new FastestValidator();
-const schema = {
-  fullName: {
-    type: "string",
-    trim: true,
-    min: 4,
-    max: 255,
-    messages: {
-      required: "نام و نام خانوادگی الزامی است.",
-      stringMin: "نام و نام خانوادگی نباید کمتر از ۴ باشد.",
-      stringMax: "نام و نام خانوادگی نباید بیشتر از ۲۵۵ باشد.",
-    },
-  },
-  email: {
-    type: "email",
-    normalize: true,
-    messages: {
-      required: "آدرس ایمیل الزامی هست.",
-      string: "آدرس ایمیل صحیح نیست.",
-      emailEmpty: "ایمیلتو بنویس.",
-    },
-  },
-  password: {
-    type: "string",
-    min: 4,
-    max: 255,
-    messages: {
-      required: "رمز عبور الزامی هست.",
-      string: "کلمه عبور را درست بنویس.",
-      stringMin: "کلمه عبور نباید کمتر از ۴ کاراکتر باشد",
-      stringMax: "کلمه عبور نباید بیشتر از ۲۵۵ باشد.",
-    },
-  },
-  repeatPassword: {
-    type: "string",
-    min: 4,
-    max: 255,
-    messages: {
-      required: "تکرار کلمه عبور الزامی هست.",
-      string: "تکرار کلمه عبور را درست بنویس.",
-      stringMin: "تکرار کلمه عبور نباید کمتر از ۴ کاراکتر باشد",
-      stringMax: "تکرار کلمه عبور نباید بیشتر از ۲۵۵ باشد.",
-    },
-  },
-};
 
 /**
  * Page: 'pages/signup'
@@ -67,35 +19,4 @@ export const signupController = (_: Request, res: Response) => {
  */
 export const handleSignup = (_: Request, res: Response) => {
   console.log(_.body);
-
-  // Validate Data
-  const validate = validator.validate(_.body, schema);
-  const errors = [];
-
-  // When Form Valid
-  if (validate === true) {
-    const { fullname, email, password, repeatPassword } = _.body;
-
-    // Check 'repeatPassword'
-    if (password !== repeatPassword) {
-      errors.push({ message: "رمز عبور و تکرار آن یکسان نیست." });
-
-      // Render Repeat Password Error
-      return res.render("pages/signup", {
-        pageTitle: "Signup",
-        layout: "loginSignup",
-        errors,
-      });
-    }
-    res.redirect("/users/login");
-  }
-
-  // When Form Invalid
-  else {
-    res.render("pages/signup", {
-      pageTitle: "Signup",
-      layout: "loginSignup",
-      errors: validate,
-    });
-  }
 };
