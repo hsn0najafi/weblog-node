47ae7b5b5023b560284ac369ff35e3396d9d94e1
hsn8najafi <hsn8najafi@gmail.com>
2022-01-08 18:14:32 +0330

    Yup Schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
+import { object, ref, string } from "yup";
 
+/**
+ * MongoDB Schema
+ */
 const UserSchema = new mongoose.Schema({
   fullName: {
     type: String,
@@ -21,6 +25,34 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+/**
+ * Yup Schema
+ */
+const schema = object().shape({
+  fullName: string()
+    //
+    .min(4, "نام و نام خانوادگی نباید کمتر از ۴ کاراکتر باشد.")
+    .max(255, ".نام و نام خانوادگی نباید بیشتر از ۲۵۵ کاراکتر باشد")
+    .required("نام و نام خانوادگی الزامی هست."),
+  //
+  email: string()
+    //
+    .email("ایمیل معتبر نیست")
+    .required("ایمیل الزامی هست."),
+  //
+  password: string()
+    //
+    .min(4, "رمز عبور باید بیشتر از ۴ کاراکتر باشد.")
+    .max(255, "کلمه عبور نباید بیشتر از ۲۵۵ تا باشه.")
+    .required("کلمه عبور باید وارد شود"),
+  //
+  repeatPassword: string()
+    //
+    .required("تکرار کلمه عبور باید وارد شود")
+    .oneOf([ref("password"), null], "تکرار کلمه عبور صحیح نیست"),
+  //
 });
 
 export const User = mongoose.model("User", UserSchema);
