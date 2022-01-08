b0411df9731829b0c2421e6606121ccfb66e67e1
hsn8najafi <hsn8najafi@gmail.com>
2022-01-08 14:54:26 +0330

    Custom Error Messages

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,4 +1,31 @@
 import { Request, Response } from "express";
+import { object, ref, string } from "yup";
+
+// Validator Schema And Error Messages
+const schema = object().shape({
+  fullName: string()
+    //
+    .min(4, "نام و نام خانوادگی نباید کمتر از ۴ کاراکتر باشد.")
+    .max(255, ".نام و نام خانوادگی نباید بیشتر از ۲۵۵ کاراکتر باشد")
+    .required("نام و نام خانوادگی الزامی هست."),
+  //
+  email: string()
+    //
+    .email("ایمیل معتبر نیست")
+    .required("ایمیل الزامی هست."),
+  //
+  password: string()
+    //
+    .min(4, "رمز عبور باید بیشتر از ۴ کاراکتر باشد.")
+    .max(255, "کلمه عبور نباید بیشتر از ۲۵۵ تا باشه.")
+    .required("کلمه عبور باید وارد شود"),
+  //
+  repeatPassword: string()
+    //
+    .required("تکرار کلمه عبور باید وارد شود")
+    .oneOf([ref("password"), null], "تکرار کلمه عبور صحیح نیست"),
+  //
+});
 
 /**
  * Page: 'pages/signup'
