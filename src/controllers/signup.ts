e1a54202e2f77061032c65ecfaffe7ae0e62160f
hsn8najafi <hsn8najafi@gmail.com>
2022-01-09 14:50:24 +0330

    Refactoring

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -22,16 +22,27 @@ export const signupController = (_: Request, res: Response) => {
 export const handleSignup = async (_: Request, res: Response) => {
   try {
     await User.userValidation(_.body);
+
+    /**
+     * if Form Valid - Redirect to Login Page
+     */
     res.redirect("/users/login");
-  } catch (err) {
-    const errors = [];
-    err.inner.map((e) => {
+  } catch (err: any) {
+    const errors: any[] = [];
+
+    /**
+     * Push Errors to a Array and then Show it's
+     */
+    err.inner.map((e: any) => {
       errors.push({
         name: e.path,
         message: e.message,
       });
     });
 
+    /**
+     * ReRender Register Page for Show Errors
+     */
     res.render("pages/signup", {
       pageTitle: "Signup",
       layout: "loginSignup",
