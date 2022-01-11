b538c12e6c5deb5dceca25ec04fadc4519286a23
hsn8najafi <hsn8najafi@gmail.com>
2022-01-11 22:22:42 +0330

    Handle Login Welcome Message

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -9,7 +9,7 @@ import { Request, Response } from "express";
 export const loginController = (_: Request, res: Response) => {
   res.render("pages/login", {
     pageTitle: "Login",
-    message: _.flash("successfullLoginMessage"),
+    message: _.flash("successfullLoginMessage") || "hghsggfyewgayw",
     layout: "loginSignup",
   });
 };
