30b73bb583427bf15014455682228f8e3953c005
hsn8najafi <hsn8najafi@gmail.com>
2022-01-11 14:33:01 +0330

    Delete UnUsed Prop

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -12,7 +12,6 @@ import { User } from "../models/User";
 export const signupController = (_: Request, res: Response) => {
   res.render("pages/signup", {
     pageTitle: "Signup",
-    message: "Register",
     layout: "loginSignup",
     errors: ["hghsggfyewgayw"],
   });
