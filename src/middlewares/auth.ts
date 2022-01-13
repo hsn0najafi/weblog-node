4333bf8db1a568a4a6d5284c33ff271a1b8e85a4
hsn8najafi <hsn8najafi@gmail.com>
2022-01-13 23:18:09 +0330

    Show Message When User not Authenticated and Want Private URLS

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,6 +4,7 @@ export const auth = (_: Request, res: Response, next: NextFunction) => {
   if (_.isAuthenticated()) {
     return next();
   } else {
+    _.flash("successMessage", "اول باید لاگین کنی");
     res.redirect("/users/login");
   }
 };
