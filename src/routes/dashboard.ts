0a0ab369ec11c43894da5ff92d0b3cf3196f332b
hsn8najafi <hsn8najafi@gmail.com>
2022-01-16 14:31:52 +0330

    Show Blogs Router

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -4,6 +4,7 @@ import {
   dashboardController,
   newPost,
   handleNewPost,
+  blogs,
 } from "../controllers/dashboard";
 import { auth } from "../middlewares/auth";
 
@@ -29,5 +30,12 @@ router.get("/add-post", auth, newPost);
  * @host           /admin/add-post
  */
 router.post("/add-post", auth, handleNewPost);
+
+/**
+ * @description    Handle New Post
+ * @method         POST
+ * @host           /admin/blogs
+ */
+router.get("/blogs", auth, blogs);
 
 module.exports = router;
