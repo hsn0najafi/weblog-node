09d2e3f5f46ced7aa88dd84f8d7846abcac8104f
hsn8najafi <hsn8najafi@gmail.com>
2022-01-18 11:03:04 +0330

    Old Browsers Support

--- src/public/scripts/main.ts
+++ src/public/scripts/main.ts
@@ -4,30 +4,32 @@
  * @param classes             Host of The Operation
  * @param togglableClasses    Addables and Removables
  */
-const classNameToggler = (
+function classNameToggler(
   eventHandlers: string[],
   classes: string[],
   togglableClasses: string[]
-): void => {
+): void {
   // Toggle Classes
-  const toggle = () => {
-    for (let b = 0; b < classes.length; b++) {
+  function toggle() {
+    for (var b = 0; b < classes.length; b++) {
       document
         .querySelector("." + classes[b])!
         .classList.toggle(togglableClasses[b]);
     }
-  };
+  }
 
   // Main
   if (togglableClasses.length === classes.length) {
-    for (let a = 0; a < eventHandlers.length; a++) {
-      const targetEl = document.querySelector("." + eventHandlers[a])!;
-      targetEl!.addEventListener("click", () => toggle());
+    for (var a = 0; a < eventHandlers.length; a++) {
+      var targetEl = document.querySelector("." + eventHandlers[a])!;
+      targetEl!.addEventListener("click", function () {
+        toggle();
+      });
     }
   } else {
     throw new Error("Hey, ClassNames List Should be Equal");
   }
-};
+}
 
 /**
  * Hamburger Menu
