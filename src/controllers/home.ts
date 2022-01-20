2ee1b589f8d38279240196e970e4c909c68fa92e
hsn8najafi <hsn8najafi@gmail.com>
2022-01-20 22:53:18 +0330

    Home Page Controller

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,18 +1,33 @@
 import { Request, Response } from "express";
 
+import { Blog } from "../models/Blog";
+import { dateToJalali } from "../utils/jm";
+import { get500 } from "./errorController";
+
 /**
  * @description    Home Page
  * @pages          pages/home
  * @layout         layout - Default(This Optional)
  */
-export const homeController = (_: Request, res: Response) => {
+export const homeController = async (_: Request, res: Response) => {
   let userFullName: string;
   _.user ? (userFullName = _.user!.fullName) : (userFullName = "no User");
 
-  res.render("pages/home", {
-    pageTitle: "Home",
-    message: "Hello",
-    layout: "layout",
-    userFullName,
-  });
+  try {
+    const posts = await Blog.find({ status: "عمومی" }).sort({
+      createDate: "desc",
+    });
+
+    res.render("pages/home", {
+      pageTitle: "Home",
+      layout: "layout",
+      userFullName,
+      dateToJalali,
+      path: "/home",
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    get500(_, res);
+  }
 };
